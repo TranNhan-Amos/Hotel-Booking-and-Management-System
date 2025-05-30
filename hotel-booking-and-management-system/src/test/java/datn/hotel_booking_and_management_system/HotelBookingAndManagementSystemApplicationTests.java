a3bb8b2c5d76c8976cc234fa48de89ec292f6686
nhan-jgsdikd <animehistwo@gmail.com>
2025-05-30 13:15:41 +0700

    Upload du an

---
+++ hotel-booking-and-management-system/src/test/java/datn/hotel_booking_and_management_system/HotelBookingAndManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package datn.hotel_booking_and_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingAndManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

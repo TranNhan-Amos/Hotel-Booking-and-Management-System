d50b97178e6d0e5eb71b90c724bf492d3b943b7d
nhan-jgsdikd <animehistwo@gmail.com>
2025-05-31 19:07:53 +0700

    Update

---
+++ hotel-booking-and-management-system/src/main/java/datn/hotel_booking_and_management_system/Entity/User.java
@@ -0,0 +1,17 @@
+package datn.hotel_booking_and_management_system.Entity;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    
+
+    
+}

e9adcb3f2f4246e4d59afe00c69e733db02bdf3d
nhan-jgsdikd <animehistwo@gmail.com>
2025-06-06 23:20:48 +0700

    loi nhan

---
+++ Myweb/hotel-booking-and-management-system/src/main/java/sd19303no1/hotel_booking_and_management_system/Repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package sd19303no1.hotel_booking_and_management_system.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sd19303no1.hotel_booking_and_management_system.Entity.ReviewEntity;
+
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
+}
---
+++ Myweb/hotel-booking-and-management-system/src/main/java/sd19303no1/hotel_booking_and_management_system/Repository/StatusRepository.java
@@ -0,0 +1,8 @@
+package sd19303no1.hotel_booking_and_management_system.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sd19303no1.hotel_booking_and_management_system.Entity.StatusEntity;
+
+public interface StatusRepository extends JpaRepository<StatusEntity, Integer> {
+}

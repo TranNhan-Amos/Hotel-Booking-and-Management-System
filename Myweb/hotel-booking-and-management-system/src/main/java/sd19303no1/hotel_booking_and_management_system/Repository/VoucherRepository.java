b7561d7360ea5c869452cba63faf82c0986bfa14
Nhan-Amos <animehistwo@gmail.com>
2025-06-06 23:23:48 +0700

    Delete Myweb/hotel-booking-and-management-system directory

--- Myweb/hotel-booking-and-management-system/src/main/java/sd19303no1/hotel_booking_and_management_system/Repository/VoucherRepository.java
+++
@@ -1,8 +0,0 @@
-package sd19303no1.hotel_booking_and_management_system.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sd19303no1.hotel_booking_and_management_system.Entity.VoucherEntity;
-
-public interface VoucherRepository extends JpaRepository<VoucherEntity, Integer> {
-}

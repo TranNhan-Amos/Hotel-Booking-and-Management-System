0d0a65f11388efc79b96a9e7c028dc2d45b3bcaa
Hoanghuyen041025 <huyenhttpd10557@gmail.com>
2025-06-08 22:26:38 +0700

    huyen

--- hotel-booking-and-management-system/src/main/java/sd19303no1/hotel_booking_and_management_system/Entity/CustomersEntity.java
+++ hotel-booking-and-management-system/src/main/java/sd19303no1/hotel_booking_and_management_system/Entity/CustomersEntity.java
@@ -1,77 +1,77 @@
-package sd19303no1.hotel_booking_and_management_system.Entity;
+    package sd19303no1.hotel_booking_and_management_system.Entity;
 
-import jakarta.persistence.*;
+    import jakarta.persistence.*;
 
-@Entity
-@Table(name = "customers")
-public class CustomersEntity {
+    @Entity
+    @Table(name = "customers")
+    public class CustomersEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
-    private Integer customerId;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "customer_id")
+        private Integer customerId;
 
-    @Column(name = "password")
-    private String password;
+        @Column(name = "password")
+        private String password;
 
-    @Column(name = "address")
-    private String address;
+        @Column(name = "address")
+        private String address;
 
-    @Column(name = "name")
-    private String name;
+        @Column(name = "name")
+        private String name;
 
-    @Column(name = "phone")
-    private String phone;
+        @Column(name = "phone")
+        private String phone;
 
-    @Column(name = "email", unique = true)
-    private String email;
+        @Column(name = "email", unique = true)
+        private String email;
 
-    // Getters and Setters
-    public Integer getCustomerId() {
-        return customerId;
+        // Getters and Setters
+        public Integer getCustomerId() {
+            return customerId;
+        }
+
+        public void setCustomerId(Integer customerId) {
+            this.customerId = customerId;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
-
-    public void setCustomerId(Integer customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
